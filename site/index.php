15950864ae0cfd97869fe58f1cbae50625f21f3e
kang <hankangkang66@163.com>
2016-09-19 01:50:19 -0700

    搞成自己的配置用

---
+++ site/index.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "mysql";
+$username = "root";
+$password = "123456";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("连接错误: " . $conn->connect_error);
+}
+echo "<h1>成功连接 MySQL 服务器</h1>";
+
+phpinfo();
+
+?>
